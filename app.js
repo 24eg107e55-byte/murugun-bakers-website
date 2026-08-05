c7e0f59da8de4f2e10707a6090eee43c47f17ed5
24eg107e55-byte <24eg107e55@anurag.edu.in>
2026-08-05 03:15:38 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function validate() {
+    const name = document.getElementById("name");
+    const tel = document.getElementById("phone");
+    const password = document.getElementById("password");
+
+    const namepattern = /^[A-Za-z ]{3,15}$/;
+    const phonepattern = /^[0-9]{10}$/;
+    const passwordpattern = /^[A-Za-z0-9]+$/;
+
+    if (!namepattern.test(name)) {
+        alert("Name should contain 3 to 15 characters");
+    }
+    else if (!phonepattern.test(tel)) {
+        alert("Phone number should contain exactly 10 digits");
+    }
+    else if (!passwordpattern.test(password)) {
+        alert("Password should contain only A-Z, a-z and 0-9");
+    }
+    else {
+        alert("Registration successful!");
+    }
+}
